f59f2247e2ef2f75828b1e55a1800b1b94ee3d09
Ana Rebelo <anagmrebelo@hotmail.com>
2023-08-03 13:53:09 +0200

    Ony allow get requests

--- src/db.ts
+++ src/db.ts
@@ -1,27 +1,31 @@
-export interface DbItem {
-  // sketch out interface here
+export interface DbTask {
+  value: string;
+  dueDate: string;
+  status: boolean;
 }
 
-export interface DbItemWithId extends DbItem {
+export interface DbTaskWithId extends DbTask {
   id: number;
 }
 
-const db: DbItemWithId[] = [];
+const db: DbTaskWithId[] = [];
 
-/** Variable to keep incrementing id of database items */
+/** Variable to keep incrementing id of database tasks */
 let idCounter = 0;
 
 /**
- * Adds in some dummy database items to the database
+ * Adds in some dummy database tasks to the database
  *
- * @param n - the number of items to generate
- * @returns the created items
+ * @param n - the number of tasks to generate
+ * @returns the created tasks
  */
-export const addDummyDbItems = (n: number): DbItemWithId[] => {
-  const createdSignatures: DbItemWithId[] = [];
+export const addDummyDbTasks = (n: number): DbTaskWithId[] => {
+  const createdSignatures: DbTaskWithId[] = [];
   for (let count = 0; count < n; count++) {
-    const createdSignature = addDbItem({
-      // possibly add some generated data here
+    const createdSignature = addDbTask({
+      value: "Call Mom",
+      dueDate: "2023-07-07",
+      status: false,
     });
     createdSignatures.push(createdSignature);
   }
@@ -29,13 +33,13 @@ export const addDummyDbItems = (n: number): DbItemWithId[] => {
 };
 
 /**
- * Adds in a single item to the database
+ * Adds in a single task to the database
  *
- * @param data - the item data to insert in
- * @returns the item added (with a newly created id)
+ * @param data - the task data to insert in
+ * @returns the task added (with a newly created id)
  */
-export const addDbItem = (data: DbItem): DbItemWithId => {
-  const newEntry: DbItemWithId = {
+export const addDbTask = (data: DbTask): DbTaskWithId => {
+  const newEntry: DbTaskWithId = {
     id: ++idCounter,
     ...data,
   };
@@ -44,31 +48,31 @@ export const addDbItem = (data: DbItem): DbItemWithId => {
 };
 
 /**
- * Deletes a database item with the given id
+ * Deletes a database task with the given id
  *
- * @param id - the id of the database item to delete
- * @returns the deleted database item (if originally located),
+ * @param id - the id of the database task to delete
+ * @returns the deleted database task (if originally located),
  *  otherwise the string `"not found"`
  */
-export const deleteDbItemById = (id: number): DbItemWithId | "not found" => {
-  const idxToDeleteAt = findIndexOfDbItemById(id);
+export const deleteDbTaskById = (id: number): DbTaskWithId | "not found" => {
+  const idxToDeleteAt = findIndexOfDbTaskById(id);
   if (typeof idxToDeleteAt === "number") {
-    const itemToDelete = getDbItemById(id);
+    const taskToDelete = getDbTaskById(id);
     db.splice(idxToDeleteAt, 1); // .splice can delete from an array
-    return itemToDelete;
+    return taskToDelete;
   } else {
     return "not found";
   }
 };
 
 /**
- * Finds the index of a database item with a given id
+ * Finds the index of a database task with a given id
  *
- * @param id - the id of the database item to locate the index of
- * @returns the index of the matching database item,
+ * @param id - the id of the database task to locate the index of
+ * @returns the index of the matching database task,
  *  otherwise the string `"not found"`
  */
-const findIndexOfDbItemById = (id: number): number | "not found" => {
+const findIndexOfDbTaskById = (id: number): number | "not found" => {
   const matchingIdx = db.findIndex((entry) => entry.id === id);
   // .findIndex returns -1 if not located
   if (matchingIdx !== -1) {
@@ -79,21 +83,21 @@ const findIndexOfDbItemById = (id: number): number | "not found" => {
 };
 
 /**
- * Find all database items
- * @returns all database items from the database
+ * Find all database tasks
+ * @returns all database tasks from the database
  */
-export const getAllDbItems = (): DbItemWithId[] => {
+export const getAllDbTasks = (): DbTaskWithId[] => {
   return db;
 };
 
 /**
- * Locates a database item by a given id
+ * Locates a database task by a given id
  *
- * @param id - the id of the database item to locate
- * @returns the located database item (if found),
+ * @param id - the id of the database task to locate
+ * @returns the located database task (if found),
  *  otherwise the string `"not found"`
  */
-export const getDbItemById = (id: number): DbItemWithId | "not found" => {
+export const getDbTaskById = (id: number): DbTaskWithId | "not found" => {
   const maybeEntry = db.find((entry) => entry.id === id);
   if (maybeEntry) {
     return maybeEntry;
@@ -103,19 +107,19 @@ export const getDbItemById = (id: number): DbItemWithId | "not found" => {
 };
 
 /**
- * Applies a partial update to a database item for a given id
+ * Applies a partial update to a database task for a given id
  *  based on the passed data
  *
- * @param id - the id of the database item to update
+ * @param id - the id of the database task to update
  * @param newData - the new data to overwrite
- * @returns the updated database item (if one is located),
+ * @returns the updated database task (if one is located),
  *  otherwise the string `"not found"`
  */
-export const updateDbItemById = (
+export const updateDbTaskById = (
   id: number,
-  newData: Partial<DbItem>
-): DbItemWithId | "not found" => {
-  const idxOfEntry = findIndexOfDbItemById(id);
+  newData: Partial<DbTask>
+): DbTaskWithId | "not found" => {
+  const idxOfEntry = findIndexOfDbTaskById(id);
   // type guard against "not found"
   if (typeof idxOfEntry === "number") {
     return Object.assign(db[idxOfEntry], newData);
--- src/server.ts
+++ src/server.ts
@@ -2,18 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import {
-  addDummyDbItems,
-  addDbItem,
-  getAllDbItems,
-  getDbItemById,
-  DbItem,
-  updateDbItemById,
+  addDummyDbTasks,
+  addDbTask,
+  getAllDbTasks,
+  getDbTaskById,
+  DbTask,
+  updateDbTaskById,
 } from "./db";
 import filePath from "./filePath";
 
-// loading in some dummy items into the database
+// loading in some dummy tasks into the database
 // (comment out if desired, or change the number)
-addDummyDbItems(20);
+addDummyDbTasks(5);
 
 const app = express();
 
@@ -28,30 +28,30 @@ dotenv.config();
 // use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
-// API info page
-app.get("/", (req, res) => {
-  const pathToFile = filePath("../public/index.html");
-  res.sendFile(pathToFile);
-});
+// // API info page
+// app.get("/", (req, res) => {
+//   const pathToFile = filePath("../public/index.html");
+//   res.sendFile(pathToFile);
+// });
 
-// GET /items
-app.get("/items", (req, res) => {
-  const allSignatures = getAllDbItems();
+// GET /tasks
+app.get("/tasks", (req, res) => {
+  const allSignatures = getAllDbTasks();
   res.status(200).json(allSignatures);
 });
 
-// POST /items
-app.post<{}, {}, DbItem>("/items", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdSignature = addDbItem(postData);
-  res.status(201).json(createdSignature);
-});
+// // POST /tasks
+// app.post<{}, {}, DbTask>("/tasks", (req, res) => {
+//   // to be rigorous, ought to handle non-conforming request bodies
+//   // ... but omitting this as a simplification
+//   const postData = req.body;
+//   const createdSignature = addDbTask(postData);
+//   res.status(201).json(createdSignature);
+// });
 
-// GET /items/:id
-app.get<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
+// GET /tasks/:id
+app.get<{ id: string }>("/tasks/:id", (req, res) => {
+  const matchingSignature = getDbTaskById(parseInt(req.params.id));
   if (matchingSignature === "not found") {
     res.status(404).json(matchingSignature);
   } else {
@@ -59,25 +59,25 @@ app.get<{ id: string }>("/items/:id", (req, res) => {
   }
 });
 
-// DELETE /items/:id
-app.delete<{ id: string }>("/items/:id", (req, res) => {
-  const matchingSignature = getDbItemById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // DELETE /tasks/:id
+// app.delete<{ id: string }>("/tasks/:id", (req, res) => {
+//   const matchingSignature = getDbTaskById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
-// PATCH /items/:id
-app.patch<{ id: string }, {}, Partial<DbItem>>("/items/:id", (req, res) => {
-  const matchingSignature = updateDbItemById(parseInt(req.params.id), req.body);
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // PATCH /tasks/:id
+// app.patch<{ id: string }, {}, Partial<DbTask>>("/tasks/:id", (req, res) => {
+//   const matchingSignature = updateDbTaskById(parseInt(req.params.id), req.body);
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
