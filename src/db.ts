bb3811d8de0a61a2b2b838b720e1d2f806485f8a
Ana Rebelo <anagmrebelo@hotmail.com>
2023-08-12 15:10:33 +0200

    Implement get and post tasks

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,20 @@
+import { Client } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config(); //read any .env file(s)
+
+if (!process.env.DATABASE_URL) {
+  throw "No DATABASE_URL env var provided.  Did you create an .env file?";
+}
+
+const config = {
+  connectionString: process.env.DATABASE_URL,
+};
+
 export interface DbTask {
+  user_id: number;
   value: string;
-  dueDate: string;
+  due_date: string;
   status: boolean;
 }
 
@@ -8,28 +22,16 @@ export interface DbTaskWithId extends DbTask {
   id: number;
 }
 
-const db: DbTaskWithId[] = [];
-
-/** Variable to keep incrementing id of database tasks */
-let idCounter = 0;
-
 /**
- * Adds in some dummy database tasks to the database
- *
- * @param n - the number of tasks to generate
- * @returns the created tasks
+ * Find all database tasks
+ * @returns all database tasks from the database
  */
-export const addDummyDbTasks = (n: number): DbTaskWithId[] => {
-  const createdSignatures: DbTaskWithId[] = [];
-  for (let count = 0; count < n; count++) {
-    const createdSignature = addDbTask({
-      value: "Call Mom",
-      dueDate: "2023-07-07",
-      status: false,
-    });
-    createdSignatures.push(createdSignature);
-  }
-  return createdSignatures;
+export const getAllDbTasks = async (): Promise<DbTaskWithId[]> => {
+  const client = new Client(config);
+  await client.connect();
+  const res = await client.query("SELECT * from tasks");
+  await client.end();
+  return res.rows.reverse();
 };
 
 /**
@@ -38,104 +40,100 @@ export const addDummyDbTasks = (n: number): DbTaskWithId[] => {
  * @param data - the task data to insert in
  * @returns the task added (with a newly created id)
  */
-export const addDbTask = (data: DbTask): DbTaskWithId => {
-  const newEntry: DbTaskWithId = {
-    id: ++idCounter,
-    ...data,
-  };
-  db.push(newEntry);
-  return newEntry;
+export const addDbTask = async (data: DbTask) => {
+  const client = new Client(config);
+  const { user_id, value, due_date, status } = data;
+
+  await client.connect();
+  const text =
+    "INSERT INTO tasks (user_id, value, due_date, status) VALUES($1, $2, $3, $4) RETURNING user_id, value, due_date, status";
+  const values = [user_id, value, due_date, status];
+  const res = await client.query(text, values);
+  await client.end();
+
+  return res.rows[0];
 };
 
-/**
- * Deletes a database task with the given id
- *
- * @param id - the id of the database task to delete
- * @returns the deleted database task (if originally located),
- *  otherwise the string `"not found"`
- */
-export const deleteDbTaskById = (id: number): DbTaskWithId | "not found" => {
-  const idxToDeleteAt = findIndexOfDbTaskById(id);
-  if (typeof idxToDeleteAt === "number") {
-    const taskToDelete = getDbTaskById(id);
-    db.splice(idxToDeleteAt, 1); // .splice can delete from an array
-    return taskToDelete;
-  } else {
-    return "not found";
-  }
-};
+// /**
+//  * Deletes a database task with the given id
+//  *
+//  * @param id - the id of the database task to delete
+//  * @returns the deleted database task (if originally located),
+//  *  otherwise the string `"not found"`
+//  */
+// export const deleteDbTaskById = (id: number): DbTaskWithId | "not found" => {
+//   const idxToDeleteAt = findIndexOfDbTaskById(id);
+//   if (typeof idxToDeleteAt === "number") {
+//     const taskToDelete = getDbTaskById(id);
+//     db.splice(idxToDeleteAt, 1); // .splice can delete from an array
+//     return taskToDelete;
+//   } else {
+//     return "not found";
+//   }
+// };
 
-/**
- * Finds the index of a database task with a given id
- *
- * @param id - the id of the database task to locate the index of
- * @returns the index of the matching database task,
- *  otherwise the string `"not found"`
- */
-const findIndexOfDbTaskById = (id: number): number | "not found" => {
-  const matchingIdx = db.findIndex((entry) => entry.id === id);
-  // .findIndex returns -1 if not located
-  if (matchingIdx !== -1) {
-    return matchingIdx;
-  } else {
-    return "not found";
-  }
-};
+// /**
+//  * Finds the index of a database task with a given id
+//  *
+//  * @param id - the id of the database task to locate the index of
+//  * @returns the index of the matching database task,
+//  *  otherwise the string `"not found"`
+//  */
+// const findIndexOfDbTaskById = (id: number): number | "not found" => {
+//   const matchingIdx = db.findIndex((entry) => entry.id === id);
+//   // .findIndex returns -1 if not located
+//   if (matchingIdx !== -1) {
+//     return matchingIdx;
+//   } else {
+//     return "not found";
+//   }
+// };
 
-/**
- * Find all database tasks
- * @returns all database tasks from the database
- */
-export const getAllDbTasks = (): DbTaskWithId[] => {
-  const db_reverse = [...db].reverse();
-  return db_reverse;
-};
+// /**
+//  * Find all database tasks that are incomplete
+//  * @returns all database tasks from the database
+//  */
+// export const getIncompleteDbTasks = (): DbTaskWithId[] => {
+//   const db_reverse_filtered = [...db]
+//     .filter((oneTask) => !oneTask.status)
+//     .reverse();
+//   return db_reverse_filtered;
+// };
 
-/**
- * Find all database tasks that are incomplete
- * @returns all database tasks from the database
- */
-export const getIncompleteDbTasks = (): DbTaskWithId[] => {
-  const db_reverse_filtered = [...db]
-    .filter((oneTask) => !oneTask.status)
-    .reverse();
-  return db_reverse_filtered;
-};
+// /**
+//  * Locates a database task by a given id
+//  *
+//  * @param id - the id of the database task to locate
+//  * @returns the located database task (if found),
+//  *  otherwise the string `"not found"`
+//  */
+// export const getDbTaskById = (id: number): DbTaskWithId | "not found" => {
+//   const maybeEntry = db.find((entry) => entry.id === id);
+//   if (maybeEntry) {
+//     return maybeEntry;
+//   } else {
+//     return "not found";
+//   }
+// };
 
-/**
- * Locates a database task by a given id
- *
- * @param id - the id of the database task to locate
- * @returns the located database task (if found),
- *  otherwise the string `"not found"`
- */
-export const getDbTaskById = (id: number): DbTaskWithId | "not found" => {
-  const maybeEntry = db.find((entry) => entry.id === id);
-  if (maybeEntry) {
-    return maybeEntry;
-  } else {
-    return "not found";
-  }
-};
-
-/**
- * Applies a partial update to a database task for a given id
- *  based on the passed data
- *
- * @param id - the id of the database task to update
- * @param newData - the new data to overwrite
- * @returns the updated database task (if one is located),
- *  otherwise the string `"not found"`
- */
-export const updateDbTaskById = (
-  id: number,
-  newData: Partial<DbTask>
-): DbTaskWithId | "not found" => {
-  const idxOfEntry = findIndexOfDbTaskById(id);
-  // type guard against "not found"
-  if (typeof idxOfEntry === "number") {
-    return Object.assign(db[idxOfEntry], newData);
-  } else {
-    return "not found";
-  }
-};
+// /**
+//  * Applies a partial update to a database task for a given id
+//  *  based on the passed data
+//  *
+//  * @param id - the id of the database task to update
+//  * @param newData - the new data to overwrite
+//  * @returns the updated database task (if one is located),
+//  *  otherwise the string `"not found"`
+//  */
+// export const updateDbTaskById = (
+//   id: number,
+//   newData: Partial<DbTask>
+// ): DbTaskWithId | "not found" => {
+//   const idxOfEntry = findIndexOfDbTaskById(id);
+//   // type guard against "not found"
+//   if (typeof idxOfEntry === "number") {
+//     return Object.assign(db[idxOfEntry], newData);
+//   } else {
+//     return "not found";
+//   }
+// };
--- src/server.ts
+++ src/server.ts
@@ -1,33 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import {
-  addDummyDbTasks,
-  addDbTask,
-  getAllDbTasks,
-  getDbTaskById,
-  DbTask,
-  updateDbTaskById,
-  getIncompleteDbTasks,
-  deleteDbTaskById,
-} from "./db";
-import filePath from "./filePath";
-
-// loading in some dummy tasks into the database
-// (comment out if desired, or change the number)
-addDummyDbTasks(5);
-
-interface Option {
-  sort: boolean;
-  filter: boolean;
-}
-
-type OptionUnion = "filter" | "sort";
-
-const options: Option = {
-  sort: false,
-  filter: false,
-};
+import { getAllDbTasks, addDbTask, DbTaskWithId, DbTask } from "./db";
 
 const app = express();
 
@@ -42,69 +16,107 @@ dotenv.config();
 // use the environment variable PORT, or 4000 as a fallback
 const PORT_NUMBER = process.env.PORT ?? 4000;
 
-// API info page
-app.get("/", (req, res) => {
-  const pathToFile = filePath("../public/index.html");
-  res.sendFile(pathToFile);
-});
-
 // GET /tasks
 app.get("/tasks", (req, res) => {
-  let allSignaturesAfterOptions: DbTask[] = [];
-  if (options.filter) {
-    allSignaturesAfterOptions = getIncompleteDbTasks();
-  } else {
-    allSignaturesAfterOptions = getAllDbTasks();
-  }
-  res.status(200).json(allSignaturesAfterOptions);
+  getAllDbTasks().then((response: DbTaskWithId[]) =>
+    res.status(200).json(response)
+  );
 });
 
 // POST /tasks
 app.post<{}, {}, DbTask>("/tasks", (req, res) => {
-  // to be rigorous, ought to handle non-conforming request bodies
-  // ... but omitting this as a simplification
-  const postData = req.body;
-  const createdSignature = addDbTask(postData);
-  res.status(201).json(createdSignature);
+  const postData: DbTask = req.body;
+  addDbTask(postData).then((createdTask) => res.status(201).json(createdTask));
 });
 
-// GET /tasks/:id
-app.get<{ id: string }>("/tasks/:id", (req, res) => {
-  const matchingSignature = getDbTaskById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // DELETE /tasks/:id
+// app.delete<{ id: string }>("/tasks/:id", (req, res) => {
+//   const matchingSignature = getDbTaskById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     deleteDbTaskById(parseInt(req.params.id));
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
-// DELETE /tasks/:id
-app.delete<{ id: string }>("/tasks/:id", (req, res) => {
-  const matchingSignature = getDbTaskById(parseInt(req.params.id));
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    deleteDbTaskById(parseInt(req.params.id));
-    res.status(200).json(matchingSignature);
-  }
-});
-
-app.patch<{}, {}, { type: OptionUnion }>("/options", (req, res) => {
-  const optionType = req.body.type;
-  options[optionType] = !options[optionType];
-  res.status(200).json({ optionType: options[optionType] });
-});
-
-// PATCH /tasks/:id
-app.patch<{ id: string }, {}, Partial<DbTask>>("/tasks/:id", (req, res) => {
-  const matchingSignature = updateDbTaskById(parseInt(req.params.id), req.body);
-  if (matchingSignature === "not found") {
-    res.status(404).json(matchingSignature);
-  } else {
-    res.status(200).json(matchingSignature);
-  }
-});
+// // GET /tasks/:id
+// app.get<{ id: string }>("/tasks/:id", (req, res) => {
+//   const matchingSignature = getDbTaskById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
 
 app.listen(PORT_NUMBER, () => {
   console.log(`Server is listening on port ${PORT_NUMBER}!`);
 });
+
+// // GET /tasks
+// app.get("/tasks", (req, res) => {
+//   let allSignaturesAfterOptions: DbTask[] = [];
+//   if (options.filter) {
+//     allSignaturesAfterOptions = getIncompleteDbTasks();
+//   } else {
+//     allSignaturesAfterOptions = getAllDbTasks();
+//   }
+//   res.status(200).json(allSignaturesAfterOptions);
+// });
+
+// // API info page
+// app.get("/", (req, res) => {
+//   // console.log("Got here!");
+//   doDemo();
+//   res.status(200).json({ status: true });
+// });
+
+// // GET /tasks/:id
+// app.get<{ id: string }>("/tasks/:id", (req, res) => {
+//   const matchingSignature = getDbTaskById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
+
+// // DELETE /tasks/:id
+// app.delete<{ id: string }>("/tasks/:id", (req, res) => {
+//   const matchingSignature = getDbTaskById(parseInt(req.params.id));
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     deleteDbTaskById(parseInt(req.params.id));
+//     res.status(200).json(matchingSignature);
+//   }
+// });
+
+// app.patch<{}, {}, { type: OptionUnion }>("/options", (req, res) => {
+//   const optionType = req.body.type;
+//   options[optionType] = !options[optionType];
+//   res.status(200).json({ optionType: options[optionType] });
+// });
+
+// // PATCH /tasks/:id
+// app.patch<{ id: string }, {}, Partial<DbTask>>("/tasks/:id", (req, res) => {
+//   const matchingSignature = updateDbTaskById(parseInt(req.params.id), req.body);
+//   if (matchingSignature === "not found") {
+//     res.status(404).json(matchingSignature);
+//   } else {
+//     res.status(200).json(matchingSignature);
+//   }
+// });
+
+// interface Option {
+//   sort: boolean;
+//   filter: boolean;
+// }
+
+// type OptionUnion = "filter" | "sort";
+
+// const options: Option = {
+//   sort: false,
+//   filter: false,
+// };
